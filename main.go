373178537296a3806bd25af436d32ae25289aa38
pro7357 <pro7357@pm.me>
2021-05-14 01:00:36 +0800

    initial commit

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"flag"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/chromedp/chromedp"
+)
+
+var (
+	userJS string
+	proxy  string
+)
+
+func init() {
+	flag.StringVar(&userJS, "j", `[...new Set([... document.links].map(n => n.href))].join("\n")`, "the JS to run on each page")
+	flag.StringVar(&proxy, "p", os.Getenv("HTTPS_PROXY"), "proxy setting")
+
+	log.SetOutput(os.Stdout)
+	log.SetFlags(0)
+}
+
+func gurl(url string) {
+	allocCtx, cancel := chromedp.NewExecAllocator(context.Background())
+	if strings.HasPrefix(proxy, "socks5:") {
+		opts := append(chromedp.DefaultExecAllocatorOptions[:],
+			chromedp.ProxyServer(proxy),
+		)
+		allocCtx, cancel = chromedp.NewExecAllocator(context.Background(), opts...)
+		defer cancel()
+	}
+	ctx, cancel := chromedp.NewContext(allocCtx)
+	defer cancel()
+
+	var res string
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(url),
+		chromedp.Evaluate(userJS, &res),
+	)
+	if err != nil {
+		log.Printf("error on %s : %s", url, err)
+	}
+
+	log.Println(res)
+}
+
+func main() {
+	flag.Parse()
+
+	urls := flag.Args()
+	if len(urls) != 0 {
+		for _, url := range urls {
+			gurl(url)
+		}
+	} else {
+		sc := bufio.NewScanner(os.Stdin)
+		for sc.Scan() {
+			url := sc.Text()
+			gurl(url)
+		}
+	}
+}
