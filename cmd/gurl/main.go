582eb094cb2ff1a69986e66283a1223820366267
pro7357 <72558903+pro7357@users.noreply.github.com>
2021-05-14 21:28:43 +0800

    Update and rename main.go to cmd/gurl/main.go
    gurl is now also a module?

---
+++ cmd/gurl/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"os"
+	
+	"github.com/pro7357/gurl"
+)
+
+func main() {
+	flag.Parse()
+
+	urls := flag.Args()
+	if len(urls) != 0 {
+		for _, url := range urls {
+			gurl.Gurl(url)
+		}
+	} else {
+		sc := bufio.NewScanner(os.Stdin)
+		for sc.Scan() {
+			url := sc.Text()
+			gurl.Gurl(url)
+		}
+	}
+}
